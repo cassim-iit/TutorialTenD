5e8593e51205b5782e87a8616be04728485fef21
Thaveesha <thaveesha.20240908@iit.ac.lk>
2024-12-06 01:49:43 +0530

    committee

--- committeemanager.py
+++ committeemanager.py
@@ -1,12 +1,9 @@
-""""
-https://www.sqlitetutorial.net/sqlite-create-table/
-"""
 import sqlite3 as db
 from entity.committee import Committee
 
 
 def initializeDatabase():
-    sql = "CREATE TABLE IF NOT EXISTS committee(commetteeName TEXT PRIMARY KEY, iD  INT)"
+    sql = "CREATE TABLE IF NOT EXISTS committee(commetteeID INT PRIMARY KEY, committeeName  TEXT )"
     con = db.connect("database\\committee.db")
     cur = con.cursor()
     cur.execute(sql)
@@ -14,84 +11,81 @@ def initializeDatabase():
     cur.close()
     con.close()
 
-initializeDatabase()
-
-
-def insertCommittee(committeeName, iD):
+def insertCommittee(committeeID, committeeName):
     oCommittee = Committee()
-    oCommitteee.committee = committeeName
-    oCommittee.id = iD
+    oCommittee.committeeid = committeeID
+    oCommittee.committeename = committeeName
     oCommittee.save()
-'''
-def removeUser(userName):
-    oUser = User()
-    oUser.username = userName
-    oUser.delete()
 
-def updateUser(userName, passWord):
-    oUser = User()
-    oUser.username = userName
-    oUser.password = passWord
-    oUser.update()
+def removeCommittee(committeeID):
+    oCommittee = Committee()
+    oCommittee.committeeid = committeeID
+    oCommittee.delete()
 
-def getUser(userName):
-    oUser = User()
-    oUser.username = userName
-    oUser.load()
-    return oUser
+def updateCommittee(committeeID, committeeName):
+    oCommittee = Committee()
+    oCommittee.committeeid = committeeID
+    oCommittee.committeename = committeeName
+    oCommittee.update()
 
-def getAllUsers():
-    oUsers = []
-    sql = "SELECT userName, passWord FROM user"
-    con = db.connect("database\\login.db")
+def getCommittee(committeeName):
+    oCommittee = Committee()
+    oCommittee.committeename = committeeName
+    oCommittee.load()
+    return oCommittee
+
+def getAllCommittees():
+    oCommittees = []
+    sql = "SELECT committeeID, committeeName FROM committee"
+    con = db.connect("database\\committee.db")
     cur = con.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
     cur.close()
     con.close()
     for row in rows:
-        oUser = User()
-        oUser.username = row[0]
-        oUser.password = row[1]
-        oUsers.append(oUser)
-    return oUsers
+        oCommittee = Committee()
+        oCommittee.committeeid = row[0]
+        oCommittee.committeename = row[1]
+        oCommittees.append(oCommittee)
+    return oCommittees
 
-def printUserList(oUsers):
-    for oUser in oUsers:
-        print(oUser.username, oUser.password)
+def printCommitteeList(oCommittees):
+    for oCommittee in oCommittees:
+        print(oCommittee.committeeid, oCommittee.committeename)
 
 def menu():
     x = 0;
     while (x != 9):
-        print("1. Show all users\n"
-              "2. Add new user\n"
-              "3. Remove user\n"
-              "4. Update user\n"
+        print("1. Show all committees\n"
+              "2. Add new committee\n"
+              "3. Remove committee\n"
+              "4. Update committee\n"
               "9. Exit")
         x = int(input("Enter your choice: "))
         match x:
             case 1:
-                oList = getAllUsers()
-                printUserList(oList)
+                oList = getAllCommittees()
+                printCommitteeList(oList)
             case 2:
-                userName = input("Enter your username: ")
-                passWord = input("Enter your password: ")
-                insertUser(userName, passWord)
-                print("User added successfully")
+                committeeID = input("Enter new committee id: ")
+                committeeName = input("Enter new committee name: ")
+                insertCommittee(committeeID,committeeName)
+                print("Committee added successfully")
             case 3:
-                userName = input("Enter your username: ")
-                removeUser(userName)
-                print("User removed successfully")
+                committeeID = input("Enter committee id: ")
+                removeCommittee(committeeID)
+                print("Committee removed successfully")
             case 4:
-                userName = input("Enter your username: ")
-                passWord = input("Enter your password: ")
-                updateUser(userName, passWord)
-                print("User updated successfully")
+                committeeID = input("Enter committee id: ")
+                committeeName = input("Enter committee name: ")
+                updateCommittee(committeeID,committeeName)
+                print("Committee updated successfully")
 
 #main for this module
 initializeDatabase()
 menu()
-'''
+
 
 
 
--- entity/committee.py
+++ entity/committee.py
@@ -2,27 +2,47 @@ import sqlite3 as db
 from math import trunc
 
 class Committee:
-    def __init__(self, committeename = "",  id = ""):
-        self.committeename = committeename
-        self.id = id
+    def __init__(self, committee_id = "",  committee_name = ""):
+        self.committeeid = committee_id
+        self.committeename = committee_name
 
     def save(self):
-        sql = "INSERT INTO committee (committeeName, iD) VALUES (?, ?)"
+        sql = "INSERT INTO committee (committeeID, committeeName) VALUES (?, ?)"
         con = db.connect("database\\committee.db")
         cur = con.cursor()
-        cur.execute(sql, (self.committeename, self.id))
+        cur.execute(sql, (self.committeeid, self.committeename))
         con.commit()
         cur.close()
         con.close()
 
     def load(self):
-        sql = "SELECT committeeName,iD FROM committee WHERE committeeName = ?"
+        sql = "SELECT committeeID,committeeName FROM committee WHERE committeeName = ?"
         con = db.connect("database\\committee.db")
         cur = con.cursor()
         cur.execute(sql, (self.committeename,))
         row = cur.fetchone()
         cur.close()
         con.close()
-        self.committeename = row[0]
-        self.id = row[1]
+        self.committeeid = row[0]
+        self.committeename = row[1]
+        return True
+
+    def delete(self):
+        sql = "DELETE FROM user WHERE committeeID = ?"
+        con = db.connect("database\\login.db")
+        cur = con.cursor()
+        cur.execute(sql, (self.committeeid,))
+        con.commit()
+        cur.close()
+        con.close()
+        return True
+
+    def update(self):
+        sql = "UPDATE user SET committeeName = ? WHERE committeeID = ?"
+        con = db.connect("database\\login.db")
+        cur = con.cursor()
+        cur.execute(sql, (self.committeename, self.committeeid))
+        con.commit()
+        cur.close()
+        con.close()
         return True
