bc538327e4041d04bcad0bab029a4c7e39ea3446
YasiruMethmal123 <yasiru.methmal@outlook.com>
2024-12-05 10:29:30 +0530

    initialize voter

--- votermanager.py
+++ votermanager.py
@@ -2,7 +2,7 @@ import sqlite3 as db
 from entity.voter import voter
 
 def initializeDatabase():
-    sql = "CREATE TABLE IF NOT EXISTS user(iitnumber TEXT PRIMARY KEY, name TEXT,district TEXT,nic TEXT ,age TEXT)"
+    sql = "CREATE TABLE IF NOT EXISTS voter(iitnumber TEXT PRIMARY KEY, name TEXT,district TEXT,nic TEXT ,age TEXT)"
     con = db.connect("database\\login.db")
     cur = con.cursor()
     cur.execute(sql)
