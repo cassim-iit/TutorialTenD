55d50edd055797b8387e75c8a79173e3648570cd
kaveesha82 <kaveeshahewage0@gmail.com>
2024-12-05 10:36:46 +0530

    1

--- housemanager.py
+++ housemanager.py
@@ -9,3 +9,14 @@ def create_table() :
 
 create_table()
 
+import sqlite3
+def create_table() :
+    con = sqlite3.connect('database\\house.db')
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS house(houseID TEXT NOT NULL UNIQUE PRIMARY KEY, houseName TEXT NOT NULL)")
+    con.commit()
+    cur.close()
+    con.close()
+
+create_table()
+
