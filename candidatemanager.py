1f334b81c4b9670532da1e84f7a67f09caf3e5cd
chanukaWijeratna <20232021.chanuka@iit.ac.lk>
2024-12-05 10:16:05 +0530

    commit

--- candidatemanager.py
+++ candidatemanager.py
@@ -9,7 +9,8 @@ def create_table() :
 create_table()
 
 class Candidate:
-    def __init__(self, candidateName, candidateAge, gender):
+    def __init__(self, candidateID, candidateName, candidateAge, gender):
+        self.candidateID = candidateID
         self.candidateName = candidateName
         self.candidateAge = candidateAge
         self.gender = gender
