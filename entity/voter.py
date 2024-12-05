442cdf1ff7d59573ebec3ec0e5994d47eb8cc3aa
YasiruMethmal123 <yasiru.methmal@outlook.com>
2024-12-05 10:12:58 +0530

    initialize

---
+++ entity/voter.py
@@ -0,0 +1,11 @@
+import sqlite3 as db
+
+class voter:
+    def __init__(self, iitnumber = "",name ="",  district = "",nic="",age=""):
+        self.age = age
+        self.nic = nic
+        self.district = district
+        self.name = name
+        self.iitnumber = iitnumber
+
+
