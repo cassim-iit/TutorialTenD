b101c8b2a25f10501ec90f899a0f28414710beca
chanukaWijeratna <20232021.chanuka@iit.ac.lk>
2024-12-05 10:25:16 +0530

    commit

--- candidatemanager.py
+++ candidatemanager.py
@@ -1,4 +1,6 @@
 import sqlite3
+import entity.candidate
+
 def create_table() :
     sql = "CREATE TABLE IF NOT EXISTS candidate(candidateID TEXT NOT NULL UNIQUE PRIMARY KEY, candidateName TEXT NOT NULL, candidateAge INT NOT NULL, gender TEXT NOT NULL)"
     con = sqlite3.connect('database\\candidates.db')
