cd29a6e58bab95f085d36744afef87d717687178
YasiruMethmal123 <yasiru.methmal@outlook.com>
2024-12-05 10:49:37 +0530

    initialize voterload

--- entity/voter.py
+++ entity/voter.py
@@ -8,9 +8,24 @@ class voter:
         self.name = name
         self.iitnumber = iitnumber
 
+    def load(self):
+        sql = "SELECT age,nic,name,distric FROM voter WHERE iitnumber = ?"
+        con = db.connect("database\\voter.db")
+        cur = con.cursor()
+        cur.execute(sql, (self.iitnumber,))
+        row = cur.fetchone()
+        cur.close()
+        con.close()
+        self.iitnumber = row[0]
+        self.name = row[1]
+        self.nic = row[2]
+        self.age = row[3]
+        self.district = row[4]
+        return True
+
     def update_table(self):
         sql = "UPDATE user SET name = ?, district = ?, nic = ?, age = ?  WHERE iitnumber = ?"
-        con = db.connect("database\\login.db")
+        con = db.connect("database\\voter.db")
         cur = con.cursor()
         cur.execute(sql, (self.name, self.district, self.nic, self.age))
         con.commit()
